d072af61568b1be9472031e221de970ccd6a1d76
lotny <tomasztrela87@gmail.com>
2019-12-01 23:02:19 +0100

    adding programatically generated cells

--- data.js
+++ data.js
@@ -1,36 +1,36 @@
-var data = 
-
+var chartData = new Object();
+chartData =
 [ 
     {
         name: "Resource 1",
-        activities = [
+        activities : [
             {
             id: "1",
             name: "first activity",
-            actualStart: Date.UTC(2019,12,1),
+            actualStart: new Date("December 1, 2019 12:00:00"),
             actualEnd: Date.UTC(2019,12,3)
         },{
             id: "2",
             name: "second activity",
-            actualStart: Date.UTC(2019,12,1),
+            actualStart: new Date("December 1, 2019 16:00:00"),
             actualEnd: Date.UTC(2019,12,3)
         }
         ]
     },
     {
         name: "Resource 2",
-        activities = [
+        activities : [
             {
             id: "activity2a",
             name: "first activity",
-            actualStart: Date.UTC(2019,12,1),
-            actualEnd: Date.UTC(2019,12,3)
+            actualStart: Date.UTC(2019,12,1,12),
+            actualEnd: Date.UTC(2019,12,1,16)
         },{
             id: "activity2b",
             name: "second activity",
-            actualStart: Date.UTC(2019,12,1),
-            actualEnd: Date.UTC(2019,12,3)
+            actualStart: Date.UTC(2019,12,16,30),
+            actualEnd: Date.UTC(2019,12,21)
         }
         ]
     },
-]
+];
--- simple_gantt.js
+++ simple_gantt.js
@@ -3,10 +3,17 @@
 // 
 var chart = (function(){
 
+    var loadedData;
+
     var startDate = new Date();
     var endDate = new Date();
 
-    var data;
+    var scale = 60; // should be value in miliseconds for each 60 pixels
+
+    var cellWidth = 60;
+    var timeInCell = 18e5;
+
+    
 
     get = function(){
 
@@ -20,11 +27,11 @@ var chart = (function(){
         startDate = Date.UTC(2019,12,1,12,0);
         endDate = Date.UTC(2019,12,1,24,0);
         // returns difference in miliseconds 18e5 - half an hour
-        let range = Math.abs(endDate - startDate) / 18e5; 
-        console.log(range);
+        let steps = Math.abs(endDate - startDate) / timeInCell; 
+        console.log(steps);
         //let cellTime = startDate;
         let cellTime = new Date("December 1, 2019, 12:30:00");
-        for (var x = 0; x<range; x++)
+        for (var x = 0; x < steps; x++)
         {
             let hours = cellTime.getHours() == "0" ? "00" : cellTime.getHours();
             let minutes = cellTime.getMinutes() == "0" ? "00" : cellTime.getMinutes();
@@ -34,6 +41,22 @@ var chart = (function(){
 
     }
 
+    drawResources = function(){
+        for (var x = 0; x < chartData.length; x++){
+            drawResource(chartData[x].name, chartData[x].length);
+        }
+
+    }
+
+    drawResource = function(name, numberOfActivities){
+        
+        var cell = document.createElement("div");
+        cell.setAttribute("style", "heigth:" + numberOfActivities * 30 + "px;");
+        cell.setAttribute("class", "row-resource");
+        cell.innerText = name;
+        document.getElementsByClassName("panel-left")[0].appendChild(cell);
+    }
+
     drawActivities = function(){
 
         //for (var x = 0; x < )
@@ -41,7 +64,7 @@ var chart = (function(){
 
 
     drawTimeCell = function(time){
-        let width = 60;
+        let width = 59;
         var cell = document.createElement("div");
         cell.setAttribute("style", "width:" + width);
         cell.setAttribute("class", "time-cell");
@@ -50,8 +73,12 @@ var chart = (function(){
     }
 
     drawGanttRow = function(time){
+        
+        let rowTime = new Date(startDate.getTime() + timeInCell * Math.floor(Math.random() * 100))
 
-        var margin = Math.floor((Math.random() * 1000) + 1);
+        var margin = rowTime.getHours() * cellWidth;
+
+        //var margin = Math.floor((Math.random() * 1000) + 1);
         var width  = Math.floor((Math.random() * 100) + 100 );
         var activity = document.createElement("div");
         activity.setAttribute("style", "width:" + width + "px;margin-left:" + margin);
@@ -61,12 +88,34 @@ var chart = (function(){
     }
 
     function getStartDate(data){
+        
+        for (var x = 0; x < data.length; x++){
+            if (data[x].activities === undefined) continue;
+            for (var y = 0; y < data[x].activities.length; y++){
+                startDate = getLowerDate(startDate, data[x].activities[y].actualStart)
+                
+            }
+        }
+        console.log(startDate);
+    }
 
+    function getLowerDate(startDate, actualStart){
+        
+        if (new Date(startDate) > new Date(actualStart)){
+            return actualStart;
+        } else {
+            return startDate;
+        }
+         
+    
     }
 
     init = function(data){
-        startDate = getStartDate(data);
-         
+        loadedData = data;
+        startDate = getStartDate(loadedData);
+        
+        drawTimeLine();
+        drawResources();
     }
 
     return {
