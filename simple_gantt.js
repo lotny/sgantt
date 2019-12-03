b85dc87caecaa7ab714cfb8ff409f36ece3605de
lotny <tomasztrela87@gmail.com>
2019-12-03 22:07:35 +0100

    added scaling time

--- simple_gantt.js
+++ simple_gantt.js
@@ -1,6 +1,3 @@
-
-// scale should be up to minutes
-// 
 var chart = (function(){
 
     var loadedData;
@@ -11,9 +8,31 @@ var chart = (function(){
     var scale = 60; // should be value in miliseconds for each 60 pixels
 
     var cellWidth = 60;
-    var timeInCell = 18e5;
+    var timeInCell = 18e5; //should it be miliseconds or just minutes?
+
     var leftMargin = 150;
     
+
+    setScale = function(scale){
+        console.log(scale);
+        
+        if (scale == "day"){
+            timeInCell = 432e5;
+            redraw();
+        } 
+
+        if (scale == "hour"){
+            timeInCell = 18e5;
+            redraw();
+        }
+        
+    }
+
+    redraw = function(){
+        //destroyActivities();
+        destroyTimeLine();
+        drawTimeLine();
+    }
 
     get = function(){
 
@@ -23,20 +42,36 @@ var chart = (function(){
 
     }
 
+    drawUpperTimeLine = function(){
+
+    }
+
     drawTimeLine = function(){
-        startDate = new Date("December 1, 2019, 12:00:00");
-        endDate = new Date("December 1, 2019, 23:00:00");
+        sDate = new Date(startDate);
+        sDate.setHours(sDate.getHours() + Math.round(sDate.getMinutes()/60));
+        sDate.setMinutes(0);
+        sDate.setSeconds(0);
+
+        eDate = new Date(endDate);
+        eDate.setHours(eDate.getHours() + Math.round(eDate.getMinutes()/60));
+        eDate.setMinutes(0);
+        eDate.setSeconds(0);
+
         // returns difference in miliseconds 18e5 - half an hour
-        let steps = Math.abs(endDate - startDate) / timeInCell; 
-        console.log(steps);
-        //let cellTime = startDate;
-        let cellTime = new Date("December 1, 2019, 12:30:00");
+        let steps = Math.floor(Math.abs(eDate - sDate) / timeInCell);
+        console.log("steps:" + steps + ", time in cell:" + timeInCell);
+        
+        // time of the first cell
+        let cellTime = sDate; //new Date("December 1, 2019, 12:30:00");
+
+
+
         for (var x = 0; x < steps; x++)
         {
             let hours = ("0" + cellTime.getHours()).slice(-2);
             let minutes = ("0" + cellTime.getMinutes()).slice(-2);
             drawTimeCell(hours + ":" + minutes);
-            cellTime = new Date(cellTime.getTime() + 1800000);
+            cellTime = new Date(cellTime.getTime() + timeInCell);
         }
 
     }
@@ -60,6 +95,19 @@ var chart = (function(){
         
     }
 
+
+    destroyActivities = function(){
+        var activities = document.getElementsByClassName("row-activity");
+        //for (var x = 0; x < activities.length; x++){
+        //    activities[x].remove();
+        //}
+    }
+
+    destroyTimeLine = function(){
+        var timeline = document.getElementsByClassName("timeline")[0];
+        timeline.innerHTML = "";
+    }
+
     drawActivities = function(activities){
         for (var x = 0; x < activities.length; x++){
             drawSingleActivity(activities[x]);
@@ -72,58 +120,52 @@ var chart = (function(){
         var actEnd = new Date(activity.actualEnd);
         var margin =  leftMargin + (1 + actStart.getHours() - startDate.getHours()) * cellWidth;
 
-
         var width  = cellWidth * (actEnd.getHours() - actStart.getHours()); 
         activityEl.setAttribute("style", "width:" + width + "px;margin-left:" + margin);
         activityEl.setAttribute("class", "row-activity");
         activityEl.innerText = activity.name;
 
-        document.getElementsByClassName("panel-right")[0].appendChild(activityEl);
+        document.getElementsByClassName("activities")[0].appendChild(activityEl);
     }
 
 
-    drawTimeCell = function(time){
+    drawTimeCell = function(timeText){
         let width = 59;
         var cell = document.createElement("div");
         cell.setAttribute("style", "width:" + width);
         cell.setAttribute("class", "time-cell");
-        cell.innerText = time;
+        cell.innerText = timeText;
         document.getElementsByClassName("timeline")[0].appendChild(cell);
     }
 
-    drawGanttRow = function(time){
-        
-        let rowTime = new Date(startDate.getTime() + timeInCell * Math.floor(Math.random() * 100))
 
-        var margin = rowTime.getHours() * cellWidth;
 
-        //var margin = Math.floor((Math.random() * 1000) + 1);
-        var width  = Math.floor((Math.random() * 100) + 100 );
-        var activity = document.createElement("div");
-        activity.setAttribute("style", "width:" + width + "px;margin-left:" + margin);
-        activity.setAttribute("class", "row-activity");
-        activity.innerText = "activity";
-        return activity;
-    }
-
-    function getStartDate(data){
+    function getDateRange(data){
         
         for (var x = 0; x < data.length; x++){
             if (data[x].activities === undefined) continue;
             for (var y = 0; y < data[x].activities.length; y++){
-                startDate = getLowerDate(startDate, data[x].activities[y].actualStart)
+                startDate = getLowerDate(startDate, data[x].activities[y].actualStart);
+                endDate = getHigherDate(endDate, data[x].activities[y].actualEnd);
                 
             }
         }
-        console.log(startDate);
+    }
+
+    function getHigherDate(endDate, actualEnd){
+        if (new Date(actualEnd) > new Date(endDate)){
+            return new Date(actualEnd);
+        } else {
+            return new Date(endDate);
+        }
     }
 
     function getLowerDate(startDate, actualStart){
         
         if (new Date(startDate) > new Date(actualStart)){
-            return actualStart;
+            return new Date(actualStart);
         } else {
-            return startDate;
+            return new Date(startDate);
         }
          
     
@@ -131,16 +173,18 @@ var chart = (function(){
 
     init = function(data){
         loadedData = data;
-        startDate = getStartDate(loadedData);
+        getDateRange(loadedData);
         
+
         drawTimeLine();
         drawResources();
     }
 
     return {
         init : init,
-        drawGanttRow : drawGanttRow,
+        
         drawTimeCell : drawTimeCell,
-        drawTimeLine : drawTimeLine
+        drawTimeLine : drawTimeLine,
+        setScale : setScale
     }
 })();
